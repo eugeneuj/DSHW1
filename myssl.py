604f1c33fb18fc8ee2abcea33edb6c71a8c443d3
ujineugene <amyeugene7lee@gamil.com>
2023-04-07 19:46:41 +0900

    함수3개 .py파일에 추가(remove함수오류ㅇ)

---
+++ myssl.py
@@ -0,0 +1,124 @@
+# Node 클래스 정의
+class Node:
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+# LinkedList 클래스 정의
+class LinkedList:
+
+	# 초기화 메소드
+	def __init__(self):
+		dummy = Node("dummy")
+		self.head = dummy
+		self.tail = dummy
+
+		self.current = None
+		self.before = None
+
+		self.num_of_data = 0
+
+	# append 메소드 (insert - 맨 뒤에 노드 추가, tail과 node의 next, 데이터 개수 변경)
+	def append(self, data):
+		new_node = Node(data)
+		self.tail.next = new_node
+		self.tail = new_node
+
+		self.num_of_data += 1
+
+	# delete 메소드 (delete - current 노드 삭제, 인접 노드의 current, next 변경, 데이터 개수 변경)
+	def delete(self):
+		pop_data = self.current.data
+
+		if self.current is self.tail:
+			self.tail = self.before
+
+			# 중요 : current가 next가 아닌 before로 변경된다.
+			self.before.next = self.current.next
+			self.current = self.before 
+
+			self.num_of_data -= 1
+
+			return pop_data
+
+	# first 메소드 (search1 - 맨 앞의 노드 검색, before, current 변경)
+	def first(self):
+		# 데이터가 없는 경우 첫번째 노드도 없기 때문에 None 리턴
+		if self.num_of_data == 0: 
+			return None
+
+		self.before = self.head
+		self.current = self.head.next
+
+		return self.current.data
+
+	# next 메소드 (search2 - current 노드의 다음 노드 검색, 이전에 first 메소드가 한번은 실행되어야 함)
+	def next(self):
+		if self.current.next == None:
+			return None
+
+		self.before = self.current
+		self.current = self.current.next
+
+		return self.current.data
+
+	# size 메소드
+	def size(self):
+		return self.num_of_data 
+        
+	def traverse_all(self):
+		if self.num_of_data == 0:
+			print("None")
+			return
+
+		self.before=self.head
+		self.current=self.head.next
+		print("head->", end=' ')
+		while self.current != None:
+			print("(%d)->"%self.current.data, end='')
+			self.before=self.current
+			self.current=self.current.next
+		print("null")
+		
+	def insert_at(self,position,new_data):
+		new_node=Node(new_data)
+		self.current=self.head
+		if position<=0:
+			print("error")
+			return None
+        
+		while position!=0:
+			self.before=self.current
+			self.current=self.current.next
+			position=position-1
+			if self.current==None:
+				break		
+		self.before.next=new_node
+        
+		if self.current==None:
+			self.tail=new_node
+		new_node.next=self.current
+        
+		self.num_of_data+=1
+    
+    
+	def remove(self,key):	
+		self.current=self.head.next
+		self.before=self.head #이거 추가함
+		idx=1
+		state=False
+		while self.current!=None:
+			if self.current.data==key:
+				state=True
+				print("%d번째 원소(%d) 삭제"%(idx,key)) 
+				self.num_of_data-=1
+				self.delete()
+                 #여기 이상함..#2배로 -됨..왜지...
+            
+			self.before=self.current
+			self.current=self.current.next
+			idx+=1
+            
+		if not state:
+			print('해당하는 원소가 없습니다.')
